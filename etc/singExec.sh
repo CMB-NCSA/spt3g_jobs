07992cfedd81596c139580435285fb7c05a6c712
Felipe Menanteau <felipe@illinois.edu>
2021-11-14 10:53:50 -0600

    Adding echo commands

--- etc/singExec.sh
+++ etc/singExec.sh
@@ -2,17 +2,18 @@
 
 # This is the main list that we will loop over
 LOOP_FILE_LIST=$loop_list
-
 # Read in the whole LOOP_FILE_LIST at once and store as an array
 LOOP_LIST=($$(<$${LOOP_FILE_LIST}))
-
 # Select the index in the array using the internal variable SLURM_ARRAY_TASK_ID
 INPUTLIST=$${LOOP_LIST[$${SLURM_ARRAY_TASK_ID}]}
-
 # Define the OUTDIR
 OUTDIR=$outdir
-
 # Define the singularity image to use
 SINGULARITY_APP=$singularity_cache/$singularity_image
+
+echo "Singulary mage: $${SINGULARITY_APP}"
+echo "Input list: $${INPUTLIST}"
+echo "Running command: $${cmd_call}"
+echo "Outputs path: $${OUTDIR} "
 
 singularity exec --bind /projects $$SINGULARITY_APP $cmd_call
